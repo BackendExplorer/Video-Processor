b2c4172084e234d89b59a5a3f2821436cee47783
BackendExplorer <146496991+BackendExplorer@users.noreply.github.com>
2025-06-03 16:33:30 +0900

    Update gui.py

--- client/gui.py
+++ client/gui.py
@@ -215,7 +215,7 @@ class StreamlitApp:
             unsafe_allow_html=True
         )
 
-    # アップロードしたファイルを一時的なパスに保存する
+    # アップロードしたファイルを一時的なパスに保存して、そのパスを返す
     def get_uploaded_file(self):
         uploaded_file = st.file_uploader("", type=["mp4", "avi", "mpeg4"])
         if uploaded_file is None:
